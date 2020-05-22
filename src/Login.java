0be27af26fcc2e97cb0bb35e8bd4fae541e5c026
Piassavage <phm.maciel17@gmail.com>
2020-05-22 19:54:25 -0300

    .

--- src/Login.java
+++ src/Login.java
@@ -1,11 +1,10 @@
 import javax.swing.*;
-import javax.swing.JPanel;
 
 public class Login {
 
     public void show(){
 
-        JFrame janela = new JFrame("Login");/* É criada uma janela em JFrame que registra o login do usuário*/
+        JFrame janela = new JFrame("Jogadores");/* É criada uma janela em JFrame que registra os nomes dos jogadores*/
         janela.setSize( 300,175);/* Define o tamanho da janela de login*/
         janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);/* Define que o comando de fechar a janela, feche o aplicativo*/
         janela.setLocationRelativeTo(null);/* Faz com que a janela seja centralizada na tela*/
@@ -13,51 +12,46 @@ public class Login {
         JPanel painel = new JPanel();/* Cria um painel JPanel*/
         painel.setLayout(null);/* Deixa um layout nulo para o painel, onde se tem de definir a posição de cada componente*/
 
-        JLabel labelUsuario = new JLabel("Usuário: ");/* Cria uma texto escrito 'usuário'*/
-        labelUsuario.setBounds(20, 20, 60, 30);/* Define o tamanho e posicionamento da label de usuário*/
+        JLabel labelUsuario1 = new JLabel("Jogador 1: ");/* Cria uma texto escrito 'usuário'*/
+        labelUsuario1.setBounds(20, 20, 80, 30);/* Define o tamanho e posicionamento da label de usuário*/
 
-        JTextField textFieldUsuario = new JTextField();/* Cria um campo de texto para que o nome do usuário seja digitado*/
-        textFieldUsuario.setBounds(75, 25, 175, 20);/* Define tamanho e posicionamento do campo de texto do Usuário*/
+        JTextField textFieldUsuario1 = new JTextField();/* Cria um campo de texto para que o nome do 1º Usuário seja digitado*/
+        textFieldUsuario1.setBounds(90, 25, 175, 20);/* Define tamanho e posicionamento do campo de texto do 1º Usuário*/
 
-        JLabel labelSenha = new JLabel("Senha: ");/* Cria uma texto escrito 'senha' */
-        labelSenha.setBounds(28, 55, 60, 30);/* Define o tamanho e posicionamento da label de senha*/
+        JLabel labelUsuario2 = new JLabel("Jogador 2: ");/* Cria uma texto escrito 'senha' */
+        labelUsuario2.setBounds(20, 55, 80, 30);/* Define o tamanho e posicionamento da label de senha*/
 
-        JPasswordField textFieldSenha = new JPasswordField();/* Cria um campo de texto de senha para que a senha do usuário seja digitada*/
-        textFieldSenha.setBounds(75, 60, 175, 20);/* Define tamanho e posicionamento do campo de texto da Senha*/
+        JTextField textFieldUsuario2 = new JTextField();/* Cria um campo de texto para que o nome do 2º Usuário seja digitado*/
+        textFieldUsuario2.setBounds(90, 60, 175, 20);/* Define tamanho e posicionamento do campo de texto do 2º Usuário*/
 
         JButton botaoEntrar = new JButton("Entrar");/* Cria um botao que quando clicado, a pessoa tenha acesso ao jogo*/
         botaoEntrar.setBounds(50, 95, 85,25);/* Define o tamanho e posicionamento do botão de acessar o jogo*/
         botaoEntrar.addActionListener(evt ->{
+            janela.setVisible(false);
 
+            Jogo game = new Jogo();
+            game.show();
         });/* Cria um evento se o botão entrar for pressionado*/
 
         JButton botaoCancelar = new JButton("Cancelar");/* Cria um botao que quando clicado, a pessoa cancele o acesso ao jogo*/
         botaoCancelar.setBounds(160, 95, 90,25);/* Define o tamanho e posicionamento do botão de cancelar o acesso ao jogo*/
         botaoCancelar.addActionListener(evt ->{
-            JFrame janela2 = new JFrame();
-            janela2.setSize(100,150);
-            janela2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            janela2.setLocationRelativeTo(null);
+            janela.setVisible(false);/* Faz com que a primeira janela mostrada, fique invisível*/
 
-            JPanel painel2 = new JPanel();
-            painel2.setLayout(null);
-
-            JLabel labelJanela2 = new JLabel("Obrigado por jogar!");
-            labelJanela2.setBounds(10,20,10,50);
-
-            janela2.add(labelJanela2);
-            janela2.setVisible(true);
-            janela.setVisible(false);
+            JOptionPane.showMessageDialog(null, "       Obrigado por jogar!");/* Mostra uma mensagem ao Usuário*/
+            janela.dispose();/* Fecha o programa principal*/
         });/* Cria um evento se o botão cancelar for pressionado*/
 
-        janela.add(botaoEntrar);/* Adiciona o botão de entrar no jogo à janela*/
-        janela.add(botaoCancelar);/* Adiciona o botão de cancelar o acesso ao jogo à janela*/
-        janela.add(labelSenha);/* Adiciona a label de senha à janela*/
-        janela.add(textFieldSenha);/* Adiciona o campo de texto da senha à janela*/
-        janela.add(labelUsuario);/* Adiciona a label de usuário à janela*/
-        janela.add(textFieldUsuario);/* Adiciona o campo de texto do usuário à janela*/
-        janela.add(painel);/* Adiciona o painel à janela*/
+        painel.add(botaoEntrar);/* Adiciona o botão de entrar no jogo ao painel*/
+        painel.add(botaoCancelar);/* Adiciona o botão de cancelar o acesso ao jogo ao painel*/
+        painel.add(labelUsuario2);/* Adiciona a label de senha ao painel*/
+        painel.add(textFieldUsuario2);/* Adiciona o campo de texto da senha ao painel*/
+        painel.add(labelUsuario1);/* Adiciona a label de usuário ao painel*/
+        painel.add(textFieldUsuario1);/* Adiciona o campo de texto do usuário ao painel*/
 
+
+        janela.getRootPane().setDefaultButton(botaoEntrar);/* Atribui à tecla enter a opção de entrar*/
+        janela.add(painel);/* Adiciona o painel à janela*/
         janela.setVisible(true);/* Faz com que a janela seja visível*/
     }
 }
